978628c749e0160f5c251c52567dc3923708d2c9
iarpita-web <arpitam1711@gmail.com>
2025-09-06 02:34:33 +0530

    This commit contains changes for initial project setup

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SearchResults from './pages/SearchResults';
+import './App.css';
+
+export default function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<SearchResults />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
